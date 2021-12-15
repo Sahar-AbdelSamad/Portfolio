de85e038108a38f784383f083c4b96d1a877b85b
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2021-12-15 21:35:57 +0200

    Update details-popup-windows

--- script.js
+++ script.js
@@ -18,27 +18,33 @@ menuButton.forEach((item) => {
 // modal windows
 
 const popupBtn = document.querySelectorAll('.popupBtn');
-popupBtn.forEach((item) => { item.addEventListener('click', openPopup)});
 
 const popupTitle = [];
-document.querySelectorAll('div.flex-container > h4').forEach((item) => { popupTitle.push(item.textContent);
+document.querySelectorAll('div.flex-container > h4').forEach((item) => {
+  popupTitle.push(item.textContent);
 });
 
-const popupTechno = ["Ruby on rails", "CSS", "JavaScript", "HTML"];
+const popupTechno = ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'];
 
-const popupDescrip = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea";
+const popupDescrip = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea';
+
+const popupLive = 'https://sahar-abdelsamad.github.io/Portfolio-setup-and-mobile-version-skeleton/';
+
+const popupSource = 'https://github.com/Sahar-AbdelSamad/Portfolio-setup-and-mobile-version-skeleton';
 
 const popupObj = {};
-for ( let i=0; i < popupTitle.length;i++){
+for (let i = 0; i < popupTitle.length; i += 1) {
   popupObj[`Project${i}`] = {};
   popupObj[`Project${i}`].title = popupTitle[i];
   popupObj[`Project${i}`].technologies = popupTechno;
   popupObj[`Project${i}`].description = popupDescrip;
+  popupObj[`Project${i}`].liveLink = popupLive;
+  popupObj[`Project${i}`].sourceLink = popupSource;
 }
 
 const popupSection = document.createElement('section');
-document.body.appendChild(popupSection);
 popupSection.className = ('popup-section');
+document.body.appendChild(popupSection);
 
 const newCloseImg = document.createElement('img');
 newCloseImg.className = ('new-close-img');
@@ -48,7 +54,7 @@ popupSection.appendChild(newCloseImg);
 
 const newProjectImg = document.createElement('img');
 newProjectImg.className = ('new-project-img');
-newProjectImg.src = ('Images/popup-project.svg');
+newProjectImg.src = ('Images/project-screenshoot.png');
 newProjectImg.alt = ('project image');
 popupSection.appendChild(newProjectImg);
 
@@ -56,51 +62,45 @@ const newTitle = document.createElement('h3');
 newTitle.className = ('new-title');
 popupSection.appendChild(newTitle);
 
-const newBtns = document.createElement('ul');
+const newBtns = document.createElement('div');
 newBtns.className = ('new-btns');
 popupSection.appendChild(newBtns);
-const firstBtn = document.createElement('li');
-firstBtn.className = ('new-btns-item');
-firstBtn.innerHTML = ('<button class="button"><a href="https://sahar-abdelsamad.github.io/Portfolio-setup-and-mobile-version-skeleton/">See Live</a></button>')
+const firstBtn = document.createElement('a');
+firstBtn.href = popupObj.Project0.liveLink;
+firstBtn.className = ('button');
+firstBtn.textContent = ('See Live');
 newBtns.appendChild(firstBtn);
-const secondBtn = document.createElement('li');
-secondBtn.className = ('new-btns-item');
-secondBtn.innerHTML = ('<button class="button"><a href="https://github.com/Sahar-AbdelSamad/Portfolio-setup-and-mobile-version-skeleton">See Source</a></button>')
+const secondBtn = document.createElement('a');
+secondBtn.href = popupObj.Project0.sourceLink;
+secondBtn.className = ('button');
+secondBtn.textContent = ('See Source');
 newBtns.appendChild(secondBtn);
 
-const newTechno = document.createElement('ul');
-newTechno.className = ('new-techno');
+const newTechno = document.createElement('div');
+newTechno.classList.add('new-techno', 'tags');
 popupSection.appendChild(newTechno);
-const newTechnoItem1 = document.createElement ('li');
-newTechnoItem1.className = ('new-techno-item');
-newTechnoItem1.textContent = popupTechno[0];
-newTechno.appendChild(newTechnoItem1);
-const newTechnoItem2 = document.createElement ('li');
-newTechnoItem2.className = ('new-techno-item');
-newTechnoItem2.textContent = popupTechno[1];
-newTechno.appendChild(newTechnoItem2);
-const newTechnoItem3 = document.createElement ('li');
-newTechnoItem3.className = ('new-techno-item');
-newTechnoItem3.textContent = popupTechno[2];
-newTechno.appendChild(newTechnoItem3);
-const newTechnoItem4 = document.createElement ('li');
-newTechnoItem4.className = ('new-techno-item');
-newTechnoItem4.textContent = popupTechno[3];
-newTechno.appendChild(newTechnoItem4)
+for (let i = 0; i < popupTechno.length; i += 1) {
+  const newTechnoItem = document.createElement('h6');
+  newTechnoItem.textContent = popupObj.Project0.technologies[i];
+  newTechno.appendChild(newTechnoItem);
+}
 
 const newDescription = document.createElement('p');
 newDescription.className = ('new-description');
 newDescription.textContent = popupObj.Project0.description;
 popupSection.appendChild(newDescription);
 
-function openPopup (item) {
-    popupSection.classList.toggle('popup');
-    popupSection.classList.toggle('popup-section');
-for (let i=0; i< popupTitle.length ; i++){
-  if (item.target===popupBtn[i]){
-    newTitle.textContent = popupObj[`Project${i}`].title;
+function openPopup(item) {
+  popupSection.classList.toggle('popup');
+  popupSection.classList.toggle('popup-section');
+  for (let i = 0; i < popupTitle.length; i += 1) {
+    if (item.target === popupBtn[i]) {
+      newTitle.textContent = popupObj[`Project${i}`].title;
     }
-}
+  }
 }
 
-const closeBtn = document.querySelectorAll('.new-close-img').forEach((item) => { item.addEventListener('click', openPopup)});
+popupBtn.forEach((item) => { item.addEventListener('click', openPopup); });
+
+const closeBtn = document.querySelectorAll('.new-close-img');
+closeBtn.forEach((item) => { item.addEventListener('click', openPopup); });
