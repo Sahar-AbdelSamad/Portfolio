845eeb622af18090f366207ac812c6ec721a5063
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-07-14 18:30:54 +0300

    Add projects

--- script.js
+++ script.js
@@ -17,14 +17,13 @@ menuButton.forEach((item) => {
 
 // modal windows
 
-const popupBtn = document.querySelectorAll('.popupBtn');
 const workSection = document.querySelector('.grid-container');
 
 const projects = [
   {
     name: "Rent Me",
     description: "Rent me is a full-stack project, the front-end is a responsive app that lists houses. A user can see the details of a specific house, and reserve it by clicking on the RESERVE button. The user can also access the RESERVATIONS page from the navigation panel, and add or delete a house. The back-end contains documentation for the API made with Postman. ",
-    image: "./Images/project-screenshoot.png",
+    image: "./Images/RentMe.png",
     technologies: ["Ruby on Rails", "PostgreSQL", "React", "Redux"],
     sourcelink: "https://github.com/Sahar-AbdelSamad/house-booking-front-end",
     livelink: "https://rent-me-capstone-project.netlify.app/",
@@ -32,7 +31,7 @@ const projects = [
   {
     name: "Budget App",
     description: "The Budget app is a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what. ",
-    image: "./Images/project-screenshoot.png",
+    image: "./Images/BudgetApp.png",
     technologies: ["Ruby on Rails", "Ruby", "PostgreSQL", "Rspec"],
     sourcelink: "https://github.com/Sahar-AbdelSamad/budget-app",
     livelink: "https://my-budget-sahar.herokuapp.com/",
@@ -40,7 +39,7 @@ const projects = [
   {
     name: "Recipe App",
     description: "The Recipe allows you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe. Also, since sharing recipes is an important part of cooking the app allows you to make them public so anyone can access them.  ",
-    image: "./Images/project-screenshoot.png",
+    image: "./Images/RecipeApp.png",
     technologies: ["Ruby on Rails", "Ruby", "PostgreSQL", "Rspec"],
     sourcelink: "https://github.com/Sahar-AbdelSamad/Rails-recipe-app",
     livelink: "https://github.com/Sahar-AbdelSamad/Rails-recipe-app",
@@ -48,7 +47,7 @@ const projects = [
   {
     name: "Catalog of Stuff",
     description: "This is a Ruby Group Capstone Project meant to wrap the concepts of Ruby language and SQL Databases, creating an OOP project based on a UML diagram. It is a console app that will help us keep a record of different types of things we won: books, music albums, and games.",
-    image: "./Images/project-screenshoot.png",
+    image: "./Images/catalog-of-stuff.png",
     technologies: ["Ruby", "PostgreSQL", 'Rspec', 'OOP'],
     sourcelink: "https://github.com/Sahar-AbdelSamad/catalog-of-stuff",
     livelink: "https://github.com/Sahar-AbdelSamad/catalog-of-stuff",
@@ -56,7 +55,7 @@ const projects = [
   {
     name: "Company Profile",
     description: "This web application retrieves real live data from the Financial modeling prep API. It provides financial information about companies listed on Nasdaq 100 and Dow Jones Market indexes. All these companies are listed on the Home page. Users can filter these companies by searching for a specific company or by choosing one from the dropdown. ",
-    image: "./Images/project-screenshoot.png",
+    image: "./Images/CompanyProfile.png",
     technologies: ["React", "Redux", "Webpack", "CSS3"],
     sourcelink: "https://github.com/Sahar-AbdelSamad/company-profile",
     livelink: "https://sahar-abdelsamad.github.io/company-profile/",
@@ -64,7 +63,7 @@ const projects = [
   {
     name: "Delicious Recipes",
     description: "Delicious Recipes is a Webpack project, it retrieves information from two different APIs: Meals DB and Involvement API. The homepage lists some recipes that we can LIKE. The comment button opens a popup window, shows us more information about the recipe, and allows us to leave a comment.",
-    image: "./Images/project-screenshoot.png",
+    image: "./Images/delicious-recipes.png",
     technologies: ["JavaScript", "CSS", "WebPack", "jest"],
     sourcelink: "https://github.com/Sahar-AbdelSamad/delicious-recipes",
     livelink: "https://sahar-abdelsamad.github.io/delicious-recipes/",
@@ -99,7 +98,6 @@ popupSection.appendChild(newCloseImg);
 
 const newProjectImg = document.createElement('img');
 newProjectImg.className = ('new-project-img');
-newProjectImg.src = ('Images/project-screenshoot.png');
 newProjectImg.alt = ('project image');
 popupSection.appendChild(newProjectImg);
 
@@ -132,7 +130,8 @@ const openPopup = (item) => {
   popupSection.classList.toggle('popup-section');
   for (let i = 0; i < projects.length; i += 1) {
     if (item.target === popupBtn[i]) {
-      newTitle.textContent = "projects[i].name";
+      newTitle.textContent = `${projects[i].name}`;
+      newProjectImg.src = `${projects[i].image}`;
       newDescription.textContent = projects[i].description;
       for (let j = 0; j < projects[i].technologies.length; j += 1) {
         const newTechnoItem = document.createElement('h6');
@@ -140,13 +139,16 @@ const openPopup = (item) => {
         newTechno.appendChild(newTechnoItem);
       }
       firstBtn.href = projects[i].livelink;
-      secondBtn.href = projects[i].sourceLink;
+      secondBtn.href = projects[i].sourcelink;
     }
   }
 };
 
-popupBtn.forEach((item) => { item.addEventListener('click', openPopup); });
+const popupBtn = document.querySelectorAll('.popupBtn');
 
 const closeBtn = document.querySelectorAll('.new-close-img');
 closeBtn.forEach((item) => { item.addEventListener('click', openPopup); });
 
+popupBtn.forEach((item) => {
+  item.addEventListener('click', openPopup);
+});
